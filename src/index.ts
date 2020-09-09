e1aba7d11112da5d9945023d6ee780de42087f52
Garrett van Wageningen <ghvanw283@gmail.com>
2020-09-08 22:55:58 -0500

    wip

--- src/index.ts
+++ src/index.ts
@@ -9,40 +9,65 @@ interface Trie {
 const testAlpha = new Map("abcdefghijklmnopqrstuvwxyz -".split("").map((char, i) => [i, char]));
 
 
-function getem(lookup: Alphabet, n: number, next: string, stack: Array<{ x: number, trie: Trie }>, result: Array<string>): Array<string> {
-    const t = stack.pop();
+function getem(n: number, currentString: string, stack: Array<{ char: string, trie: Trie}>, result: Array<string>): Array<string> {
+    const item = stack.pop();
 
-    if (result.length === n || t === undefined) {
+    if (result.length === n || item === undefined) {
         return result;
     }
 
-    if (t.trie.isWord) {
-        result.push(next + lookup.get(t.x))
-        return getem(lookup, n, "", stack, result);
+    const newString = currentString + item.char;
+    if (item?.trie.isWord) {
+        result.push(newString)
+    }
+
+    if (item.trie.next.size === 0) {
+        return getem(n, newString.substring(0, newString.length - 1), stack, result);
     }
     
-    return getem(lookup, n, next + lookup.get(t.x), stack, result)
+    const letters = item.trie.next.entries();
+    for (let [char, trie] of letters) {
+        stack.push({ char, trie });
+    }
+
+    return getem(n, newString, stack, result);
+}
+
+function findSuggestions(n: number, s: string, t: Trie): Array<string> {
+    return getem(n, "", [{ char: "", trie: t }], []).map(suffix => s.concat(suffix));
+}
+
+function makeNode(): Trie {
+    return { isWord: false, next: new Map() };
 }
 
 function addWord(trieRoot: Trie, chars: string, index: number): void {
-    if (chars.charAt(index) === "") {
+    const nextChar = chars.charAt(index);
+    if (nextChar === "") {
         trieRoot.isWord = true;
         return;
     }
 
-    const nextChar = chars.charAt(index);
     const nextTries = trieRoot.next;
+    const nextNode = nextTries.get(nextChar);
+    if (nextNode !== undefined) {
+        return addWord(nextNode, chars, index + 1);
+    }
+
     const trie = { isWord: false, next: new Map() };
     nextTries.set(nextChar, trie);
 
     return addWord(trie, chars, index + 1);
 };
 
+
 // since JavaScript doesn't have a char, we have to use 'strings' to represent characters
-// function buildTrie(lookup: Alphabet, dictionary: Map<number, string>): Trie {
+function buildTrie(words: Array<string>): Trie {
+    const root = makeNode();
 
+    for (let word of words) {
+        addWord(root, word, 0);
+    }
 
-//     const root = { isWord: false, next: [] };
-//     const arrSize = dictionary.size;
-//     const entries = dictionary.values();
-// }
+    return root;
+}
